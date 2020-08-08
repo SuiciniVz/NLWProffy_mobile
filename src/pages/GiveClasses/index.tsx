f2a7419feeea7f54371e8305a0e52a636368517d
Lucas Santos <lucasparaipaba113@gmail.com>
2020-08-08 13:04:05 -0300

    Structured and stylized GiveClassesPage

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler'
+import { View, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+
+import giveClassesBackgroundImage from '../../assets/images/give-classes-background.png';
 
 import style from './style'
 
 function GiveClasses () {
 
-  const { navigate } = useNavigation();
+  const { goBack } = useNavigation();
   
   function handleNavigateToLanding () {
-    navigate('Landing')
-  }
+    goBack();
+  } 
 
   return (
-    <View>
-      <RectButton onPress={handleNavigateToLanding} style={style.button}>
-        <Text>
-          Voltar
+    <View style={style.container}>
+      <ImageBackground 
+        resizeMode="contain" 
+        source={giveClassesBackgroundImage} 
+        style={style.content}
+      >
+        <Text style={style.title}>
+          Quer ser um Proffy?
+        </Text>
+        <Text style={style.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web
+        </Text>
+
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateToLanding} style={style.okButton}>
+        <Text style={style.okButtonText}>
+          Tudo bem
         </Text>
       </RectButton>
+      
     </View>
   )
 }
--- src/pages/GiveClasses/style.ts
+++ src/pages/GiveClasses/style.ts
@@ -1,14 +1,43 @@
 import { StyleSheet } from 'react-native'
 
 const style = StyleSheet.create( {
-  button: {
-    height: 150,
-    width: '48%',
-    borderRadius: 8,  
-    padding: 24,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#121212',
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#8257E5',
+    padding: 40,
+  },
+  title: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 32,
+    lineHeight: 37,
+    maxWidth: 180,
+  },
+  description: {
+    fontFamily: 'Poppins_400Regular',
+    marginTop: 24,
+    color: '#d4c2ff',
+    fontSize: 16,
+    lineHeight: 26,
+    maxWidth: 240,
+  },
+  okButton: {
+    backgroundColor: '#04d361',
+    marginVertical: 40,
+    height: 58,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: 'center',
+  },
+  okButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontFamily: 'Archivo_700Bold',
   },
 })
 
