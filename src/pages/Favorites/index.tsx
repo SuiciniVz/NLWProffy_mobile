c1b7a555700659162d8fb2b602cc8b3ec362db1e
Lucas Santos <lucasparaipaba113@gmail.com>
2020-08-09 12:52:05 -0300

    adds the functionality to list favorite teacehrs

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem, { Teacher } from '../../components/TeacherItem'
 
 import style from './style';
 
+import AsyncStorage from '@react-native-community/async-storage';
+
 function Favorites () {
+
+  const [favorites, setFavorites] = useState([])
+
+  function loadFavoriteTeacher() {
+    
+    AsyncStorage.getItem('favorites').then(res => {
+
+      if(res) {
+        const favoritedTeachers = JSON.parse(res);
+   
+        setFavorites(favoritedTeachers)
+      }
+
+    })
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavoriteTeacher();
+    },[])
+  )
+
   return(
     <View style={style.container}>
       <PageHeader title="Meus Proffys favoritos"/>
@@ -17,7 +42,11 @@ function Favorites () {
         paddingHorizontal: 16,
         paddingBottom: 16,
       }}>
-        
+        {
+          favorites.map((teacher: Teacher) => {
+            return <TeacherItem  key={teacher.id} teacher={teacher} favorited={true} />
+          })
+        }
       </ScrollView>
       
     </View>
